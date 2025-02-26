928d2c1aad7a59eccbede5ba54e7736a75d264b5
matheus <matheus.araujo7@estudante.ifto.edu.br>
2025-02-26 10:05:55 -0300

    implementando o banco de dados

--- database/migrations/2025_02_26_121909_create_users_table.php
+++ database/migrations/2025_02_26_121909_create_users_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->string('cpf', 11)->unique();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone', 13)->unique();
+            $table->boolean('is_verified')->default(false);
+            $table->string('role'); 
             $table->timestamps();
         });
     }
--- database/migrations/2025_02_26_121944_create_entries_table.php
+++ database/migrations/2025_02_26_121944_create_entries_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('entries', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->foreignId('invoice_id')->nullable()->constrained('invoices')->onDelete('set null');
+            $table->dateTime('entry_date');
+            $table->string('entry_type'); 
+            $table->string('account_code')->nullable();
+            $table->text('description');
+            $table->decimal('amount', 15, 2);
+            $table->string('nature');
             $table->timestamps();
         });
     }
